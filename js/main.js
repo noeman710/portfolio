0b3567cb1324f17264161f7399f571ff0bf80aad
Icon <icon@users.noreply.github.com>
2026-09-23 07:41:41 +0000

    Fix map: CartoDB free tiles now require an API key, switch to standard OpenStreetMap tiles (no key ever needed) and scope the dark-mode filter to just the tile layer so the marker/card stay unaffected

--- js/main.js
+++ js/main.js
@@ -965,9 +965,8 @@ function ensureTangierMap() {
     attributionControl: true,
   }).setView(coords, 13);
 
-  L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    subdomains: 'abcd',
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 19,
   }).addTo(tangierMap);
 
